a9f792f5558aab90c22835c6bdfbb2cf3c53304c
liyh <victorli@tencentmusic.com>
2025-12-05 22:07:31 +0800

    style: 调整命名

--- rollup.config.js
+++ rollup.config.js
@@ -24,7 +24,7 @@ module.exports = [
         dir: 'lib',
         format: 'umd',
         entryFileNames: '[name].umd.js',
-        name: 'FE_utils', // umd模块名称，相当于一个命名空间，会自动挂载到window下面
+        name: 'Lhooks', // umd模块名称，相当于一个命名空间，会自动挂载到window下面
         sourcemap: false,
         plugins: [terser()],
       },
